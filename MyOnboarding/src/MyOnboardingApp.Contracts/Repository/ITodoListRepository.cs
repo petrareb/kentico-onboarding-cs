d39b61706de363c458d3e64feb9adb3ca5db2e13
Petra Rebrosova <petrar@kentico.com>
2018-09-18 16:05:55 +0200

    KA-311 Add Comparer for items and toString, create class for service testing

--- MyOnboarding/src/MyOnboardingApp.Contracts/Repository/ITodoListRepository.cs
+++ MyOnboarding/src/MyOnboardingApp.Contracts/Repository/ITodoListRepository.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using MyOnboardingApp.Content.Models;
+using MyOnboardingApp.Contracts.Models;
 
-namespace MyOnboardingApp.Content.Repository
+namespace MyOnboardingApp.Contracts.Repository
 {
     public interface ITodoListRepository
     {
