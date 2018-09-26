9a6175e54f1c92275b50e152bcd1fd8c9174a026
Petra Rebrosova <petrar@kentico.com>
2018-09-26 08:33:07 +0200

    KA-311 add extension method for using ItemEqComparer and use it in tests

---
+++ MyOnboarding/test/MyOnboardingApp.Api.Tests/Comparators/ItemEqualityExtension.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace MyOnboardingApp.Tests.Comparators
+{
+    public static class ItemEqualityExtension
+    {
+        public static EqualConstraint UsingItemEqualityComparer(this EqualConstraint constraint) 
+            => constraint.Using(new ItemEqualityComparer());
+    }
+}
