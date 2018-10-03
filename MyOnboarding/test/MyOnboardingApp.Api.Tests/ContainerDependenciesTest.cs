5b0bebeb61c2cc0e438d12b9b7ca3bbc57659bae
Petra Rebrosova <petrar@kentico.com>
2018-10-03 15:33:36 +0200

    KA-311 Use mocking via NSubstitute in container dependencies test

--- MyOnboarding/test/MyOnboardingApp.Api.Tests/ContainerDependenciesTest.cs
+++ MyOnboarding/test/MyOnboardingApp.Api.Tests/ContainerDependenciesTest.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using MyOnboardingApp.Contracts.Registration;
+using NSubstitute;
 using NUnit.Framework;
 using Unity;
-using Unity.Extension;
 using Unity.Lifetime;
 using Unity.Registration;
-using Unity.Resolution;
 
 namespace MyOnboardingApp.Api.Tests
 {
     [TestFixture]
     public class ContainerDependenciesTest
     {
-        private readonly IUnityContainer _container = new UnityContainerMock();
         private Type[] _exportedTypes;
         private readonly Type[] _ignoredTypes = 
         {
@@ -26,69 +24,47 @@ namespace MyOnboardingApp.Api.Tests
             typeof(HttpRequestMessage)
         };
 
-        [Test]
-        public void UnityContainer_AfterDependencyRegistration_ContainsAllContracts()
-        {
-            _exportedTypes = typeof(IBootstrapper)
+        public Type[] GetExportedTypesFromAssembly() 
+            => typeof(IBootstrapper)
                 .Assembly
                 .ExportedTypes
                 .Where(contract => contract.IsInterface)
                 .Except(_ignoredTypes)
                 .Union(_explicitTypes)
                 .ToArray();
-            
-            DependenciesConfig.RegisterAllDependencies(_container);
-            var actualTypes = _container
-                .Registrations
-                .Select(registration => registration.RegisteredType)
-                .ToArray();
-            var unexpectedTypes = actualTypes
-                .Except(_exportedTypes)
-                .ToArray();
-            var missingTypes = _exportedTypes
-                .Except(actualTypes)
-                .ToArray();
 
-            Assert.That(unexpectedTypes, Is.Empty);
-            Assert.That(missingTypes, Is.Empty);
+        public Type[] GetExtraTypesFromArray(Type[] array, Type[] expectedArray) 
+            => array.Except(expectedArray).ToArray();
+
+        public IUnityContainer MockUnityContainer(ICollection<Type> actualTypes)
+        {
+            var container = Substitute.For<IUnityContainer>();
+            container
+                .RegisterType(Arg.Any<Type>(), Arg.Any<Type>(), Arg.Any<string>(), Arg.Any<LifetimeManager>(), Arg.Any<InjectionMember[]>())
+                .Returns(callInfo =>
+                {
+                    var typeFrom = callInfo.ArgAt<Type>(0);
+                    var typeTo = callInfo.ArgAt<Type>(1);
+                    actualTypes.Add(typeFrom ?? typeTo);
+                    return container;
+                });
+            return container;
         }
 
-        private class UnityContainerMock: IUnityContainer
+
+        [Test]
+        public void UnityContainer_AfterDependencyRegistration_ContainsAllContracts()
         {
-            public IUnityContainer Parent { get; } = new UnityContainer();
-            public IEnumerable<IContainerRegistration> Registrations => _registrations;
-            private readonly List<IContainerRegistration> _registrations = new List<IContainerRegistration>();
-
-            public void Dispose() 
-                => throw new NotImplementedException();
-
-            public IUnityContainer RegisterType(Type typeFrom, Type typeTo, string name, LifetimeManager lifetimeManager,
-                params InjectionMember[] injectionMembers)
-            {
-                _registrations.Add(new ContainerRegistration(typeFrom, name, typeTo, lifetimeManager));
-                return this;
-            }
-
-            public IUnityContainer RegisterInstance(Type type, string name, object instance, LifetimeManager lifetime) 
-                => throw new NotImplementedException();
-
-            public object Resolve(Type type, string name, params ResolverOverride[] resolverOverrides) 
-                => throw new NotImplementedException();
-
-            public object BuildUp(Type type, object existing, string name, params ResolverOverride[] resolverOverrides) 
-                => throw new NotImplementedException();
-
-            public IUnityContainer AddExtension(UnityContainerExtension extension) 
-                => throw new NotImplementedException();
-
-            public object Configure(Type configurationInterface) 
-                => throw new NotImplementedException();
-
-            public IUnityContainer CreateChildContainer() 
-                => throw new NotImplementedException();
-
-            public bool IsRegistered(Type type, string name) 
-                => throw new NotImplementedException();
-        }   
+            _exportedTypes = GetExportedTypesFromAssembly();
+            var actualTypes = new List<Type>();
+            var container = MockUnityContainer(actualTypes);
+      
+            DependenciesConfig.RegisterAllDependencies(container);
+            var unexpectedTypes = GetExtraTypesFromArray(actualTypes.ToArray(), _exportedTypes);
+            var missingTypes = GetExtraTypesFromArray(_exportedTypes, actualTypes.ToArray());
+              
+            Assert.That(unexpectedTypes, Is.Empty, "There are more types registered to the container than expected.");
+            Assert.That(missingTypes, Is.Empty, "Some of the types are not registered.");
+        } 
     }
 }
