022e873aa021531b88d9e18811f20dc0ebd3c5bb
Petra Rebrosova <petrar@kentico.com>
2018-11-15 14:43:58 +0100

    KA-312 Manage connectionString and connection to the DB

---
+++ MyOnboarding/src/MyOnboardingApp.Api/Configuration/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using MyOnboardingApp.Contracts.Database;
+
+namespace MyOnboardingApp.Api.Configuration
+{
+    internal class DatabaseConnection : IDatabaseConnection
+    {
+        private static readonly string s_databaseConnectionString =
+            ConfigurationManager.ConnectionStrings["TodoListDbConnection"].ConnectionString;
+
+
+        public string GetDatabaseConnectionString()
+            => s_databaseConnectionString;
+    }
+}
