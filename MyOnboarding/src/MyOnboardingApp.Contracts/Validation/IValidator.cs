f7abf738c55477426b169102752bfdc919c51c3f
Petra Rebrosova <petrar@kentico.com>
2018-11-15 15:10:37 +0100

    KA-312 Add classes representing item with status and validation (validator and validating criteria)

---
+++ MyOnboarding/src/MyOnboardingApp.Contracts/Validation/IValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MyOnboardingApp.Contracts.Models;
+
+namespace MyOnboardingApp.Contracts.Validation
+{
+    public interface IValidationCriterion<in TEntity>
+        where TEntity : class
+    {
+        IEnumerable<string> Validate(TEntity entity);
+    }
+
+    public interface IValidator<TEntity>
+        where TEntity : class
+    {
+        IItemWithErrors<TEntity> Validate(TEntity item);
+    }
+}
