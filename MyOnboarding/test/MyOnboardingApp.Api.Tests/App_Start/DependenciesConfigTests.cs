2dd20ab867478990e3f78a3be2104742b6f689bd
Petra Rebrosova <petrar@kentico.com>
2018-11-15 16:49:11 +0100

    KA-312 Test dependencies

--- MyOnboarding/test/MyOnboardingApp.Api.Tests/App_Start/DependenciesConfigTests.cs
+++ MyOnboarding/test/MyOnboardingApp.Api.Tests/App_Start/DependenciesConfigTests.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using MyOnboardingApp.Contracts.Generators;
+using MyOnboardingApp.Contracts.Models;
 using MyOnboardingApp.Contracts.Registration;
+using MyOnboardingApp.Contracts.Validation;
 using NSubstitute;
 using NUnit.Framework;
 using Unity;
@@ -14,14 +17,23 @@ namespace MyOnboardingApp.Api.Tests
     [TestFixture]
     public class DependenciesConfigTests
     {
-        private static readonly Type[] s_ignoredTypes = 
+        private static readonly Type[] s_ignoredTypes =
         {
-            typeof(IBootstrapper)
+            typeof(IBootstrapper),
         };
 
         private static readonly Type[] s_explicitTypes =
         {
             typeof(HttpRequestMessage)
+        };
+
+        private static readonly Type[] s_explicitGenericTypes =
+        {
+            typeof(IIdGenerator<Guid>),
+            typeof(IValidator<TodoListItem>),
+            typeof(IValidationCriterion<TodoListItem>),
+            typeof(IResolvedItem<TodoListItem>),
+            typeof(IItemWithErrors<TodoListItem>),
         };
 
 
@@ -33,21 +45,27 @@ namespace MyOnboardingApp.Api.Tests
             var container = MockUnityContainer(actualTypes);
 
             DependenciesConfig.RegisterAllDependencies(container);
-            var unexpectedTypes = actualTypes.Except(exportedTypes).ToArray();
-            var missingTypes = exportedTypes.Except(actualTypes).ToArray();
+            var unexpectedTypes = actualTypes
+                .Except(exportedTypes)
+                .ToArray();
+            var missingTypes = exportedTypes
+                .Except(actualTypes)
+                .ToArray();
 
             Assert.That(unexpectedTypes, Is.Empty, "There are more types registered to the container than expected.");
             Assert.That(missingTypes, Is.Empty, "Some of the types are not registered.");
         }
 
 
-        private static Type[] GetTypesExportedFromAssembly() 
+        private static Type[] GetTypesExportedFromAssembly()
             => typeof(IBootstrapper)
                 .Assembly
                 .ExportedTypes
                 .Where(contract => contract.IsInterface)
                 .Except(s_ignoredTypes)
+                .Except(s_explicitGenericTypes.Select(type => type.GetGenericTypeDefinition()))
                 .Union(s_explicitTypes)
+                .Union(s_explicitGenericTypes)
                 .ToArray();
 
 
