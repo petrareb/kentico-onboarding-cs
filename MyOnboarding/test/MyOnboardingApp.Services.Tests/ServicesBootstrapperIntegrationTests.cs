35a6e2382df2bfd9962759593181b87ec5b807c2
Petra Rebrosova <petrar@kentico.com>
2018-12-04 13:42:51 +0100

    KA-312 Update NUnit and Unity in whole solution

---
+++ MyOnboarding/test/MyOnboardingApp.Services.Tests/ServicesBootstrapperIntegrationTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using MyOnboardingApp.Contracts.Models;
+using MyOnboardingApp.Contracts.Repository;
+using MyOnboardingApp.Contracts.Validation;
+using NSubstitute;
+using NUnit.Framework;
+using Unity;
+
+namespace MyOnboardingApp.Services.Tests
+{
+    [TestFixture]
+    public class ServicesBootstrapperIntegrationTests
+    {
+        public class FakeModel
+        {
+        }
+
+        private readonly IInvariantValidator<TodoListItem> _todoListItemValidator =
+            Substitute.For<IInvariantValidator<TodoListItem>>();
+
+        private readonly IInvariantValidator<FakeModel> _fakeModelValidator =
+            Substitute.For<IInvariantValidator<FakeModel>>();
+
+        private readonly ITodoListRepository _repository = 
+            Substitute.For<ITodoListRepository>();
+
+
+        [Test]
+        public void ValidateConfiguration_ContainerWithMultipleValidatorsSpecified_ThrowsException()
+        {
+            var container = new UnityContainer()
+                .RegisterInstance(_todoListItemValidator)
+                .RegisterInstance(_fakeModelValidator)
+                .RegisterInstance(_repository);
+
+            var servicesBootstrapper = new ServicesBootstrapper();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                servicesBootstrapper.ValidateConfiguration(container);
+            });
+        }
+
+
+        [Test]
+        public void ValidateConfiguration_ContainerWithOneValidatorSpecified_PassesValidation()
+        {
+            var container = new UnityContainer()
+                .RegisterInstance(_todoListItemValidator)
+                .RegisterInstance(_repository);
+          
+            var servicesBootstrapper = new ServicesBootstrapper();
+            servicesBootstrapper.Register(container);
+
+            Assert.DoesNotThrow(() =>
+            {
+                servicesBootstrapper.ValidateConfiguration(container);
+            });
+        }
+
+
+        [Test]
+        public void ValidateConfiguration_RepositoryNotRegistered_ThrowsException()
+        {
+            var container = new UnityContainer()
+                .RegisterInstance(_todoListItemValidator);
+
+            var servicesBootstrapper = new ServicesBootstrapper();
+            servicesBootstrapper.Register(container);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                servicesBootstrapper.ValidateConfiguration(container);
+            });
+        }
+    }
+}
