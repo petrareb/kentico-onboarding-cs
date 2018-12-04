9bc92dbf2a6afafdd1558f9eb029f796c257b741
Petra Rebrosova <petrar@kentico.com>
2018-12-04 15:06:27 +0100

    KA-312 Remove Bson annotation from TodoListItem

--- MyOnboarding/src/MyOnboardingApp.Contracts/Models/TodoListItem.cs
+++ MyOnboarding/src/MyOnboardingApp.Contracts/Models/TodoListItem.cs
@@ -1,27 +1,22 @@
 ﻿using System;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace MyOnboardingApp.Contracts.Models
 {
     public class TodoListItem
-    { 
-        [BsonId]
+    {
         public Guid Id { get; set; }
 
-        [BsonElement("text")]
         public string Text { get; set; }
 
-        [BsonElement("creation_time")]
         public DateTime CreationTime { get; set; }
 
-        [BsonElement("last_update_time")]
         public DateTime LastUpdateTime { get; set; }
 
 
         public override string ToString()
-        => $"{nameof(Id)}: {Id}, " +
-        $"{nameof(Text)}: {Text}, " +
-        $"{nameof(CreationTime)}: {CreationTime:0:MM/dd/yy H:mm:ss}" +
-        $"{nameof(LastUpdateTime)}: {LastUpdateTime:0:MM/dd/yy H:mm:ss}";
+            => $"{nameof(Id)}: {Id}, " +
+               $"{nameof(Text)}: {Text}, " +
+               $"{nameof(CreationTime)}: {CreationTime:0:MM/dd/yy H:mm:ss}" +
+               $"{nameof(LastUpdateTime)}: {LastUpdateTime:0:MM/dd/yy H:mm:ss}";
     }
 }
