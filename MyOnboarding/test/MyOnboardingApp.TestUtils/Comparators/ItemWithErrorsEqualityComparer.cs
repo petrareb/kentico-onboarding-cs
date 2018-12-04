dc43a63b6d499b70862fd667f8709822b0cb67ee
Petra Rebrosova <petrar@kentico.com>
2018-12-04 15:13:25 +0100

    KA-312 Add Comparator for ItemWithErrors and ResultItem

---
+++ MyOnboarding/test/MyOnboardingApp.TestUtils/Comparators/ItemWithErrorsEqualityComparer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MyOnboardingApp.Contracts.Models;
+using MyOnboardingApp.Contracts.Validation;
+
+namespace MyOnboardingApp.TestUtils.Comparators
+{
+    internal sealed class ItemWithErrorsEqualityComparer : IEqualityComparer<IItemWithErrors<TodoListItem>>
+    {
+        private static readonly Lazy<IEqualityComparer<IItemWithErrors<TodoListItem>>> s_itemWithErrorsComparer
+            = new Lazy<IEqualityComparer<IItemWithErrors<TodoListItem>>>(() => new ItemWithErrorsEqualityComparer());
+
+        public static IEqualityComparer<IItemWithErrors<TodoListItem>> Instance 
+            => s_itemWithErrorsComparer.Value;
+
+
+        private ItemWithErrorsEqualityComparer()
+        {
+        }
+
+
+        public bool Equals(IItemWithErrors<TodoListItem> x, IItemWithErrors<TodoListItem> y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (AreElementarilyDistinct(x, y))
+            {
+                return false;
+            }
+
+            return AreItemsInsideEqual(x, y)
+                   && AreOtherPropertiesEqual(x, y);
+        }
+
+
+        public int GetHashCode(IItemWithErrors<TodoListItem> obj) 
+            => obj.Item?.Id.GetHashCode() ?? obj.Errors.GetHashCode();
+
+
+        private static bool AreElementarilyDistinct(IItemWithErrors<TodoListItem> x, IItemWithErrors<TodoListItem> y) 
+            => x is null
+               || y is null
+               || x.GetType() != y.GetType();
+
+
+        private static bool AreItemsInsideEqual(IItemWithErrors<TodoListItem> x, IItemWithErrors<TodoListItem> y) 
+            => ItemEqualityComparer
+                .Instance
+                .Equals(x.Item, y.Item);
+
+
+        private static bool AreOtherPropertiesEqual(IItemWithErrors<TodoListItem> x, IItemWithErrors<TodoListItem> y) 
+            => !x.Errors.Except(y.Errors).Any()
+               && !y.Errors.Except(x.Errors).Any()
+               && y.WasOperationSuccessful == x.WasOperationSuccessful;
+    }
+}
--- MyOnboarding/test/MyOnboardingApp.TestUtils/Extensions/ItemEqualityExtensions.cs
+++ MyOnboarding/test/MyOnboardingApp.TestUtils/Extensions/ItemEqualityExtensions.cs
@@ -3,9 +3,17 @@ using NUnit.Framework.Constraints;
 
 namespace MyOnboardingApp.TestUtils.Extensions
 {
-    internal static class ItemEqualityExtensions
+    public static class ItemEqualityExtensions
     {
-        public static EqualConstraint UsingItemEqualityComparer(this EqualConstraint constraint) 
-            => constraint.Using(new ItemEqualityComparer());
+        public static EqualConstraint UsingItemEqualityComparer(this EqualConstraint constraint)
+            => constraint.Using(ItemEqualityComparer.Instance);
+
+
+        public static EqualConstraint UsingItemWithErrorsEqualityComparer(this EqualConstraint constraint)
+            => constraint.Using(ItemWithErrorsEqualityComparer.Instance);
+
+
+        public static EqualConstraint UsingResolvedItemEqualityComparer(this EqualConstraint constraint)
+            => constraint.Using(ResolvedItemEqualityComparer.Instance);
     }
 }
