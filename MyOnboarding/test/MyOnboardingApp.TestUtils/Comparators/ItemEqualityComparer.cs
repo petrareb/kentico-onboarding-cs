c253f262c41847dae1ca735b962d1e51f7a3ea49
Petra Rebrosova <petrar@kentico.com>
2018-12-04 15:13:25 +0100

    KA-312 Add Comparator for ItemWithErrors and ResultItem

--- MyOnboarding/test/MyOnboardingApp.TestUtils/Comparators/ItemEqualityComparer.cs
+++ MyOnboarding/test/MyOnboardingApp.TestUtils/Comparators/ItemEqualityComparer.cs
@@ -1,34 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MyOnboardingApp.Contracts.Models;
 
 namespace MyOnboardingApp.TestUtils.Comparators
 {
     internal sealed class ItemEqualityComparer : IEqualityComparer<TodoListItem>
     {
+        private static readonly Lazy<ItemEqualityComparer> s_itemEqualityComparer 
+            = new Lazy<ItemEqualityComparer>(() => new ItemEqualityComparer());
+
+        public static IEqualityComparer<TodoListItem> Instance 
+            => s_itemEqualityComparer.Value;
+
+
+        private ItemEqualityComparer()
+        {
+        }
+
+
         public bool Equals(TodoListItem x, TodoListItem y)
         {
-            if (ReferenceEquals(x, y)) return true;
-            if (x is null
-                || y is null
-                || x.GetType() != y.GetType())
-                return false;
-            return x.Id.Equals(y.Id)
-                   && string.Equals(x.Text, y.Text)
-                   && x.CreationTime.Equals(y.CreationTime)
-                   && x.LastUpdateTime.Equals(y.LastUpdateTime);
-        }
-
-
-        public int GetHashCode(TodoListItem obj)
-        {
-            unchecked
+            if (ReferenceEquals(x, y))
             {
-                var hashCode = obj.Id.GetHashCode();
-                hashCode = (hashCode * 397) ^ obj.Text.GetHashCode();
-                hashCode = (hashCode * 397) ^ obj.CreationTime.GetHashCode();
-                hashCode = (hashCode * 397) ^ obj.LastUpdateTime.GetHashCode();
-                return hashCode;
+                return true;
             }
+
+            return !AreElementarilyDistinct(x, y) 
+                   && ArePropertiesEqual(x, y);
         }
+
+
+        public int GetHashCode(TodoListItem obj) 
+            => obj.Id.GetHashCode();
+
+
+        private static bool AreElementarilyDistinct(TodoListItem x, TodoListItem y) 
+            => x is null
+               || y is null
+               || x.GetType() != y.GetType();
+
+
+        private static bool ArePropertiesEqual(TodoListItem x, TodoListItem y) 
+            => x.Id.Equals(y.Id)
+               && string.Equals(x.Text, y.Text)
+               && x.CreationTime.Equals(y.CreationTime)
+               && x.LastUpdateTime.Equals(y.LastUpdateTime);
     }
 }
---
+++ MyOnboarding/test/MyOnboardingApp.TestUtils/Comparators/ResolvedItemEqualityComparer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using MyOnboardingApp.Contracts.Models;
+using MyOnboardingApp.Contracts.Validation;
+
+namespace MyOnboardingApp.TestUtils.Comparators
+{
+    internal sealed class ResolvedItemEqualityComparer : IEqualityComparer<IResolvedItem<TodoListItem>>
+    {
+        private static readonly Lazy<IEqualityComparer<IResolvedItem<TodoListItem>>> s_resolvedItemComparer
+            = new Lazy<IEqualityComparer<IResolvedItem<TodoListItem>>>(() => new ResolvedItemEqualityComparer());
+
+        public static IEqualityComparer<IResolvedItem<TodoListItem>> Instance
+            => s_resolvedItemComparer.Value;
+
+
+        private ResolvedItemEqualityComparer()
+        {
+        }
+
+
+        public bool Equals(IResolvedItem<TodoListItem> x, IResolvedItem<TodoListItem> y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (AreElementarilyDistinct(x, y))
+            {
+                return false;
+            }
+
+            return AreItemsInsideEqual(x, y) 
+                   && AreOtherPropertiesEqual(x, y);
+        }
+
+
+        public int GetHashCode(IResolvedItem<TodoListItem> obj) 
+            => obj.Item?.Id.GetHashCode() 
+               ?? obj.WasOperationSuccessful.GetHashCode();
+
+
+        private static bool AreElementarilyDistinct(IResolvedItem<TodoListItem> x, IResolvedItem<TodoListItem> y)
+            => x is null
+               || y is null
+               || x.GetType() != y.GetType();
+
+
+        private static bool AreOtherPropertiesEqual(IResolvedItem<TodoListItem> x, IResolvedItem<TodoListItem> y)
+            => x.WasOperationSuccessful == y.WasOperationSuccessful;
+
+
+        private static bool AreItemsInsideEqual(IResolvedItem<TodoListItem> x, IResolvedItem<TodoListItem> y)
+            => ItemEqualityComparer
+                .Instance
+                .Equals(x.Item, y.Item);
+    }
+}
