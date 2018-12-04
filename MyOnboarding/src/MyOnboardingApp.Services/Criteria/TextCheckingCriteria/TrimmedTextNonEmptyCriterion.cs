2567c721b67a38eab2af168f28ddd34b4e0211f6
Petra Rebrosova <petrar@kentico.com>
2018-12-04 14:36:36 +0100

    KA-312 Add criteria with tests

---
+++ MyOnboarding/src/MyOnboardingApp.Services/Criteria/TextCheckingCriteria/TrimmedTextNonEmptyCriterion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using MyOnboardingApp.Contracts.ErrorFactories;
+using MyOnboardingApp.Contracts.Errors;
+using MyOnboardingApp.Contracts.Models;
+using MyOnboardingApp.Contracts.Validation;
+
+namespace MyOnboardingApp.Services.Criteria.TextCheckingCriteria
+{
+    public class TrimmedTextNonEmptyCriterion : IValidationCriterion<TodoListItem>
+    {
+        public IEnumerable<Error> Validate(TodoListItem item, IErrorFactory errorFactory)
+        {
+            if (item.Text == null)
+            {
+                yield return errorFactory.CreateValidationError(
+                    () => item.Text,
+                    "Text of the item must not be null.");
+            }
+
+            if (string.IsNullOrEmpty(item.Text))
+            {
+                yield return errorFactory.CreateValidationError(
+                    () => item.Text,
+                    "Text of the item must not be empty.");
+            }
+
+            if (string.IsNullOrWhiteSpace(item.Text))
+            {
+                yield return errorFactory.CreateValidationError(
+                    () => item.Text,
+                    "Text of the item must not be made of just whitespace characters.");
+            }
+        }
+    }
+}
