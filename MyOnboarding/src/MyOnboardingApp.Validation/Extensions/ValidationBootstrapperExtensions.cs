f6d45cfd6da3ef908d356126e0fc4c3ed45560af
Petra Rebrosova <petrar@kentico.com>
2019-02-04 12:52:30 +0100

    KA-312 Rename Validator to InvariantValidator, manage ValidationBootstrapper

---
+++ MyOnboarding/src/MyOnboardingApp.Validation/Extensions/ValidationBootstrapperExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Unity.Registration;
+
+namespace MyOnboardingApp.Validation.Extensions
+{
+    public static class ValidationBootstrapperExtensions
+    {
+        public static IEnumerable<ConstructorInfo> GetConstructorsOfMappedTypes(this IEnumerable<IContainerRegistration> registrations)
+            => registrations
+                .SelectMany(registration => registration
+                    .MappedToType
+                    .GetConstructors());
+
+
+        public static IEnumerable<ParameterInfo> GetParametersFromConstructors(this IEnumerable<ConstructorInfo> constructors)
+            => constructors
+                .SelectMany(constructor => constructor.GetParameters());
+
+
+        public static IEnumerable<Type> GetParameterTypes(this IEnumerable<ParameterInfo> parameters)
+            => parameters
+                .Select(parameter => parameter.ParameterType);
+
+
+        public static IEnumerable<Type> SelectGenericTypesImplementingType(this IEnumerable<Type> types, Type implementedType)
+            => types
+                .Where(type => type.IsGenericType
+                               && type.GetGenericTypeDefinition() == implementedType);
+
+
+        public static IEnumerable<Type> SelectArgumentsFromGenericTypes(this IEnumerable<Type> genericTypes)
+            => genericTypes
+                .Select(parameterType => parameterType.GetGenericArguments().First())
+                .Distinct();
+
+
+        public static IEnumerable<Type> GetRegisteredTypes(this IEnumerable<IContainerRegistration> registrations)
+            => registrations
+                .Select(registration => registration.RegisteredType);
+    }
+}
--- MyOnboarding/src/MyOnboardingApp.Validation/ValidationBootstrapper.cs
+++ MyOnboarding/src/MyOnboardingApp.Validation/ValidationBootstrapper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MyOnboardingApp.Contracts.ErrorFactories;
 using MyOnboardingApp.Contracts.Registration;
 using MyOnboardingApp.Contracts.Validation;
 using MyOnboardingApp.Validation.ErrorFactories;
+using MyOnboardingApp.Validation.Extensions;
 using MyOnboardingApp.Validation.Validators;
 using Unity;
 using Unity.Lifetime;
@@ -14,19 +17,72 @@ namespace MyOnboardingApp.Validation
         public IUnityContainer Register(IUnityContainer container) => container
             .RegisterType(
                 typeof(IInvariantValidator<>),
-                typeof(Validator<>),
+                typeof(InvariantValidator<>),
                 new SingletonLifetimeManager())
             .RegisterType<IErrorFactory, ErrorFactory>(new SingletonLifetimeManager());
 
 
         public void ValidateConfiguration(IUnityContainer container)
         {
+            // Check the equality of models registered for criteria and validator
+            var validatedModels = GetModelsFromRegisteredValidators(container);
+            var criteriaModels = GetModelsFromRegisteredCriteria(container);
+
+            var extraValidatorModels = validatedModels
+                .Except(criteriaModels)
+                .ToArray();
+            var extraCriteriaModels = criteriaModels
+                .Except(validatedModels)
+                .ToArray();
+
+            if (extraValidatorModels.Any())
+            {
+                var extraModelNames = GetTypeNames(extraValidatorModels);
+                throw new InvalidOperationException(
+                    $"In {nameof(IUnityContainer)} there are injected validators for models that have no {typeof(IValidationCriterion<>)} registered. " +
+                    $"The following models are validated, but miss criteria: {string.Join(", ", extraModelNames)}");
+            }
+
+            if (extraCriteriaModels.Any())
+            {
+                var extraModelNames = GetTypeNames(extraCriteriaModels);
+                throw new InvalidOperationException(
+                    $"In {nameof(IUnityContainer)} there are criteria registered for models that are not validated by an injected {typeof(IInvariantValidator<>)}." +
+                    $"The following models are not validated: {string.Join(", ", extraModelNames)}");
+            }
+
             if (!container.IsRegistered<IErrorFactory>())
             {
                 throw new InvalidOperationException(
                     $"In {nameof(IUnityContainer)} there has to be registered an array of {nameof(IErrorFactory)}, " +
-                    $"in order for a validator to create various validation errors.");
+                    $"in order for the {typeof(IInvariantValidator<>)} to create various validation errors.");
             }
         }
+
+
+        private static ICollection<Type> GetModelsFromRegisteredValidators(IUnityContainer container)
+            => container
+                .Registrations
+                .GetConstructorsOfMappedTypes()
+                .GetParametersFromConstructors()
+                .GetParameterTypes()
+                .SelectGenericTypesImplementingType(typeof(IInvariantValidator<>))
+                .SelectArgumentsFromGenericTypes()
+                .ToArray();
+
+
+        private static ICollection<Type> GetModelsFromRegisteredCriteria(IUnityContainer container)
+            => container
+                .Registrations
+                .GetRegisteredTypes()
+                .SelectGenericTypesImplementingType(typeof(IValidationCriterion<>))
+                .SelectArgumentsFromGenericTypes()
+                .ToArray();
+
+
+        private static string[] GetTypeNames(IEnumerable<Type> types) 
+            => types
+                .Select(type => type.FullName)
+                .ToArray();
     }
 }
