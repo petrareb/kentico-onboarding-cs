c507568681886f7da3cc757c23e4e44d8f3b3592
Petra Rebrosova <petrar@kentico.com>
2019-02-19 10:23:20 +0100

    KA-312 Add Error Comparator

---
+++ MyOnboarding/test/MyOnboardingApp.TestUtils/Extensions/ErrorEqualityExtensions.cs
@@ -0,0 +1,11 @@
+﻿using MyOnboardingApp.TestUtils.Comparators;
+using NUnit.Framework.Constraints;
+
+namespace MyOnboardingApp.TestUtils.Extensions
+{
+    public static class ErrorEqualityExtensions
+    {
+        public static EqualConstraint UsingErrorEqualityComparer(this EqualConstraint constraint)
+            => constraint.Using(ErrorComparer.Instance);
+    }
+}
