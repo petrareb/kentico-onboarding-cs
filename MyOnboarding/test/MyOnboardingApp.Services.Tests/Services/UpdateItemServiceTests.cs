1b908ca6b98ca80afe95832bfe956a801eb22c24
Petra Rebrosova <petrar@kentico.com>
2019-03-12 14:21:49 +0100

    KA-312 Use own type for Item Variants in services and their tests

--- MyOnboarding/test/MyOnboardingApp.Services.Tests/Services/UpdateItemServiceTests.cs
+++ MyOnboarding/test/MyOnboardingApp.Services.Tests/Services/UpdateItemServiceTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using MyOnboardingApp.Contracts.Errors;
 using MyOnboardingApp.Contracts.Generators;
@@ -31,74 +31,58 @@ namespace MyOnboardingApp.Services.Tests.Services
             dateTimeGenerator
                 .GetCurrentDateTime()
                 .Returns(updatedTime);
-            var itemToEdit = new TodoListItem
-            {
-                Id = new Guid("00000000-0000-0000-0000-000000000001"),
-                Text = "new text",
-                CreationTime = new DateTime(2015, 01, 01),
-                LastUpdateTime = new DateTime(2015, 02, 02)
-            };
-            var expectedItem = new TodoListItem
-            {
-                Id = new Guid("00000000-0000-0000-0000-000000000001"),
-                Text = "new text",
-                CreationTime = new DateTime(2015, 01, 01),
-                LastUpdateTime = updatedTime
-            };
-            repository.ReplaceItemAsync(Arg.Is<TodoListItem>(arg => arg.Id == itemToEdit.Id)).Returns(expectedItem);
-            validator.Validate(Arg.Is<TodoListItem>(item => item.Id == expectedItem.Id))
-                .Returns(ItemWithErrors.Create(expectedItem, new List<Error>().AsReadOnly()));
+            var itemId = new Guid("00000000-0000-0000-0000-000000000001");
+            var (_, existingResolvedItem, _) = ItemVariantsFactory.CreateItemVariants(
+                itemId,
+                "old text",
+                new DateTime(2015, 01, 01),
+                new DateTime(2015, 02, 02)
+            );
 
-            var resultItem = await service.EditItemAsync(itemToEdit);
+            var (newItem, _, _) = ItemVariantsFactory.CreateItemVariants(
+                itemId,
+                "new text");
 
-            await repository.Received(1).ReplaceItemAsync(expectedItem);
-            Assert.That(resultItem.Item, Is.EqualTo(expectedItem).UsingItemEqualityComparer());
-            Assert.That(resultItem.WasOperationSuccessful, Is.True);
+            var (_, _, expectedResult) = ItemVariantsFactory.CreateItemVariants(
+                itemId,
+                newItem.Text,
+                existingResolvedItem.Item.CreationTime,
+                updatedTime);
+
+           
+            repository
+                .ReplaceItemAsync(Arg.Any<TodoListItem>())
+                .Returns(callInfo => callInfo.Arg<TodoListItem>());
+            validator
+                .Validate(Arg.Any<TodoListItem>())
+                .Returns(callInfo => ItemWithErrors.Create(callInfo.Arg<TodoListItem>(), Enumerable.Empty<Error>()));
+
+            var resultItem = await service.EditItemAsync(newItem, existingResolvedItem);
+
+            Assert.Multiple(async () =>
+            {
+                Assert.That(resultItem, Is.EqualTo(expectedResult).UsingItemWithErrorsEqualityComparer());
+                await repository
+                    .Received(1)
+                    .ReplaceItemAsync(Arg.Any<TodoListItem>());
+            });
         }
 
 
         [Test]
-        public async Task EditItemAsync_InvalidItemSpecified_ReturnsItemWithErrors()
-        {
-            var repository = Substitute.For<ITodoListRepository>();
-            var dateTimeGenerator = Substitute.For<IDateTimeGenerator>();
-            var validator = Substitute.For<IInvariantValidator<TodoListItem>>();
-            var service = new UpdateItemService(repository, dateTimeGenerator, validator);
-            var updatedTime = new DateTime(2018, 10, 10);
-            dateTimeGenerator
-                .GetCurrentDateTime()
-                .Returns(updatedTime);
-            var itemToEdit = new TodoListItem
-            {
-                Id = new Guid("00000000-0000-0000-0000-000000000001"),
-                Text = string.Empty,
-                CreationTime = new DateTime(2015, 01, 01),
-                LastUpdateTime = new DateTime(2015, 02, 02)
-            };
-            var error = new Error(ErrorCode.DataValidationError, "message", "location");
-            var errors = new List<Error> { error };
-            var itemWithError = ItemVariantsFactory.CreateItemVariants(itemToEdit, errors).ItemWithErrors;
-            validator.Validate(itemToEdit).Returns(itemWithError);
-
-            var result = await service.EditItemAsync(itemToEdit);
-
-            await repository.Received(0).ReplaceItemAsync(itemToEdit);
-            Assert.That(result.WasOperationSuccessful, Is.False);
-            Assert.That(result.Errors, Is.EqualTo(errors));
-        }
-
-
-        [Test]
-        public async Task EditItemAsync_NullItemSpecified_ThrowsArgumentNullException()
+        public void EditItemAsync_NullItemSpecified_ThrowsArgumentNullException()
         {
             var repository = Substitute.For<ITodoListRepository>();
             var dateTimeGenerator = Substitute.For<IDateTimeGenerator>();
             var validator = Substitute.For<IInvariantValidator<TodoListItem>>();
             var service = new UpdateItemService(repository, dateTimeGenerator, validator);
 
-            await repository.Received(0).ReplaceItemAsync(null);
-            Assert.ThrowsAsync<ArgumentNullException>(
-                () => service.EditItemAsync(null));
+            Assert.Multiple(async () =>
+            {
+                await repository.Received(0).ReplaceItemAsync(null);
+                Assert.ThrowsAsync<ArgumentNullException>(
+                    () => service.EditItemAsync(null, Arg.Any<IResolvedItem<TodoListItem>>()));
+            });
         }
     }
 }
